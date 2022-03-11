1a42cd94b08dbaa6b38ffa9bd7b0d6fe41ded1d3
sekararumk <sekararumk21@gmail.com>
2022-03-11 15:00:12 +0700

    update soal 7

--- soal7.js
+++ soal7.js
@@ -1,42 +1,38 @@
 //  menggunakan reduce untuk mengeksekusi setiap elemen pada array 
 
 const dataPenjualanPakAldi = [
-    {
-      namaProduct : 'Sepatu Futsal Nike Vapor Academy 8',
-      hargaSatuan: 760000,
-      kategori : "Sepatu Sport",
-      totalTerjual : 90,
-    },
-    {
-      namaProduct : 'Sepatu Warrior Tristan Black Brown High',
-      hargaSatuan: 960000,
-      kategori : "Sepatu Sneaker",
-      totalTerjual : 37,
-    },
-    {
-      namaProduct : 'Sepatu Warrior Tristan Maroon High ',
-      kategori : "Sepatu Sneaker",
-      hargaSatuan: 360000,
-      totalTerjual : 90,
-    },
-    {
-      namaProduct : 'Sepatu Warrior Rainbow Tosca Corduroy',
-      hargaSatuan: 120000,
-      kategori : "Sepatu Sneaker",
-      totalTerjual : 90,
-    }
+  {
+    namaProduct: "Sepatu Futsal Nike Vapor Academy 8",
+    hargaSatuan: 760000,
+    kategori: "Sepatu Sport",
+    totalTerjual: 90,
+  },
+  {
+    namaProduct: "Sepatu Warrior Tristan Black Brown High",
+    hargaSatuan: 960000,
+    kategori: "Sepatu Sneaker",
+    totalTerjual: 37,
+  },
+  {
+    namaProduct: "Sepatu Warrior Tristan Maroon High ",
+    kategori: "Sepatu Sneaker",
+    hargaSatuan: 360000,
+    totalTerjual: 90,
+  },
+  {
+    namaProduct: "Sepatu Warrior Rainbow Tosca Corduroy",
+    hargaSatuan: 120000,
+    kategori: "Sepatu Sneaker",
+    totalTerjual: 90,
+  },
 ];
 
 // EXPECTED RESULT
 // Ketika function tersebut dipanggil dengan variabel dataPenjualanPakAldi
 const hitungTotalPenjualan = (dataPenjualan) => {
-    if ( typeof dataPenjualan === 'undefined' ) {
-        return "ERROR";
-    } else {
-    return dataPenjualan.reduce((result, item) => {
+      return dataPenjualan.reduce((result, item) => {
         return result + item.totalTerjual;
-    }, 0);
-    }
+      }, 0);
 };
 
 console.log(hitungTotalPenjualan(dataPenjualanPakAldi));
